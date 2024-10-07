8272afb3e4a5a2b6decef6fdbb8e4532b11aad8f
Romgi Productions <85715209+Romgi@users.noreply.github.com>
2024-10-07 14:54:46 -0400

    Added Test.java

---
+++ Java Files/Java Test/src/Test.java
@@ -0,0 +1,10 @@
+/*Testing written quiz from paper */
+
+public class Test {
+    public static void main(String[]skibidi){
+        int a = 1, b = 10;
+        while(a+2 < b){
+            System.out.println(a++);
+        }
+    }
+}
