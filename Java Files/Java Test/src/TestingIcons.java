c0a60f59545f67aff93986366a953a5d30a706d4
Romgi Productions <85715209+Romgi@users.noreply.github.com>
2024-10-17 15:03:38 -0400

    Testing out icons for dialogue boxes using JOptionPane

---
+++ Java Files/Java Test/src/TestingIcons.java
@@ -0,0 +1,12 @@
+//Testing java.swing icons
+
+import javax.swing.JOptionPane;
+
+public class TestingIcons {
+    public static void main(String[]args){
+        JOptionPane.showMessageDialog(null, "This is a red X icon", null, 0);
+        JOptionPane.showMessageDialog(null, "This is a blue i icon", null, 1);
+        JOptionPane.showMessageDialog(null, "This is a yellow ! icon", null, 2);
+        JOptionPane.showMessageDialog(null, "This is a green ? icon", null, 3);
+    }
+}
