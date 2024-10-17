d18fbfd73fb4ebd81a78342b35766b8176881847
Romgi Productions <85715209+Romgi@users.noreply.github.com>
2024-10-17 14:59:16 -0400

    Fixed Hurricane Classifier

--- Java Files/Java Test/src/ClassifyingHurricanes.java
+++ Java Files/Java Test/src/ClassifyingHurricanes.java
@@ -7,24 +7,24 @@
 import javax.swing.JOptionPane;
 
 public class ClassifyingHurricanes {
-    public static void main(){
+    public static void main(String[]args){
 
         String cat = JOptionPane.showInputDialog(null, "Enter the category of the hurricane: ");
         int category = Integer.parseInt(cat);
 
         switch(category){
             default:
-                JOptionPane.showMessageDialog(null, "Error. Please enter a number from 1-5", null, 0);
+                JOptionPane.showMessageDialog(null, "Error: Please enter a number from 1-5", null, 0); break;
             case 1: 
-                JOptionPane.showMessageDialog(null, "74-95 mph or 64-82 kt or 119-153 km/hr", null, 1);
+                JOptionPane.showMessageDialog(null, "Winds: 74-95 mph or 64-82 kt or 119-153 km/hr", null, 1); break;
             case 2:
-                JOptionPane.showMessageDialog(null, "96-110 mph or 83-95 kt or 154-177 km/hr", null, 1);
+                JOptionPane.showMessageDialog(null, "Winds: 96-110 mph or 83-95 kt or 154-177 km/hr", null, 1); break;
             case 3:
-                JOptionPane.showMessageDialog(null, "111-130 mph or 96-113 kt or 178-209 km/hr", null, 1);
+                JOptionPane.showMessageDialog(null, "Winds: 111-130 mph or 96-113 kt or 178-209 km/hr", null, 1); break;
             case 4:
-                JOptionPane.showMessageDialog(null, "131-155 mph or 114-135 kt or 210-249 km/hr", null, 1);
+                JOptionPane.showMessageDialog(null, "Winds: 131-155 mph or 114-135 kt or 210-249 km/hr", null, 1); break;
             case 5:
-                JOptionPane.showMessageDialog(null, "greater than 155 mph or 135 kt or 249 km/hr", null, 1);
+                JOptionPane.showMessageDialog(null, "Winds: greater than 155 mph or 135 kt or 249 km/hr", null, 1); break;
         }
         
     }
