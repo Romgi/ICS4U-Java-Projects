3b2f4b394fe941793bc8f77e6e5143b848616e21
Romgi Productions <JonathanGraydon22@gmail.com>
2024-10-22 14:17:57 -0400

    finished Average Calculator program with additional error checking for invalid inputs

--- Java Files/Java Test/src/AverageCalculatorErrorChecking.java
+++ Java Files/Java Test/src/AverageCalculatorErrorChecking.java
@@ -12,18 +12,18 @@ public class AverageCalculatorErrorChecking {
         boolean isError = false;
 
         while(!isError){
-            System.out.println("Enter grade 1: "); int grade1 = sc.nextInt(); if(grade1 < 0 || grade1 > 100){isError = true; break;}
-            System.out.println("Enter grade 2: "); int grade2 = sc.nextInt(); if(grade2 < 0 || grade1 > 100){isError = true; break;}
-            System.out.println("Enter grade 3: "); int grade3 = sc.nextInt(); if(grade3 < 0 || grade1 > 100){isError = true; break;}
-            System.out.println("Enter grade 4: "); int grade4 = sc.nextInt(); if(grade4 < 0 || grade1 > 100){isError = true; break;}
+            System.out.println("Enter grade 1: "); double grade1 = sc.nextDouble(); if(grade1 < 0 || grade1 > 100){isError = true; break;}
+            System.out.println("Enter grade 2: "); double grade2 = sc.nextDouble(); if(grade2 < 0 || grade1 > 100){isError = true; break;}
+            System.out.println("Enter grade 3: "); double grade3 = sc.nextDouble(); if(grade3 < 0 || grade1 > 100){isError = true; break;}
+            System.out.println("Enter grade 4: "); double grade4 = sc.nextDouble(); if(grade4 < 0 || grade1 > 100){isError = true; break;}
 
-            int average = (grade1 + grade2 + grade3 + grade4) /4;
-            System.out.println("The average is: " + average);
+            double average = (grade1 + grade2 + grade3 + grade4) /4;
+            System.out.println("The average is: " + average + "%");
             break;
         }
 
-        if(!isError){
-            System.out.println("Enter a value between 0 and 100");
+        if(isError){
+            System.out.println("Enter a numeric value between 0 and 100");
         }
 
 
