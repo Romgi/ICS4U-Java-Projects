46e916260b70f5fdb076868de105baae25b2dd91
Romgi Productions <85715209+Romgi@users.noreply.github.com>
2024-10-25 14:30:36 -0400

    initialized pig dice

---
+++ Java Files/Java Test/src/PigDice.java
@@ -0,0 +1,12 @@
+/*
+ * Jonathan Graydon
+ * Pig Dice Game
+ * This program will play the pig dice game with the user
+ */
+
+
+public class PigDice {
+    public static void main(String[] args) {
+        
+    }
+}
