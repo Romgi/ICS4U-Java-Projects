14d9278b52ac2b6320de941a4f19f39c49b6e522
Romgi Productions <85715209+Romgi@users.noreply.github.com>
2024-10-25 14:01:58 -0400

    Fixed average calculator with error checking

--- Java Files/Java Test/src/AverageCalculatorErrorChecking.java
+++ Java Files/Java Test/src/AverageCalculatorErrorChecking.java
@@ -13,9 +13,9 @@ public class AverageCalculatorErrorChecking {
 
         while(!isError){
             System.out.println("Enter grade 1: "); double grade1 = sc.nextDouble(); if(grade1 < 0 || grade1 > 100){isError = true; break;}
-            System.out.println("Enter grade 2: "); double grade2 = sc.nextDouble(); if(grade2 < 0 || grade1 > 100){isError = true; break;}
-            System.out.println("Enter grade 3: "); double grade3 = sc.nextDouble(); if(grade3 < 0 || grade1 > 100){isError = true; break;}
-            System.out.println("Enter grade 4: "); double grade4 = sc.nextDouble(); if(grade4 < 0 || grade1 > 100){isError = true; break;}
+            System.out.println("Enter grade 2: "); double grade2 = sc.nextDouble(); if(grade2 < 0 || grade2 > 100){isError = true; break;}
+            System.out.println("Enter grade 3: "); double grade3 = sc.nextDouble(); if(grade3 < 0 || grade3 > 100){isError = true; break;}
+            System.out.println("Enter grade 4: "); double grade4 = sc.nextDouble(); if(grade4 < 0 || grade4 > 100){isError = true; break;}
 
             double average = (grade1 + grade2 + grade3 + grade4) /4;
             System.out.println("The average is: " + average + "%");
