e77d4f7f77c23d4cbc7834586ddb6f2ea2fdab4b
Romgi Productions <85715209+Romgi@users.noreply.github.com>
2024-10-30 13:29:08 -0400

    sbigidi

---
+++ Java Files/Java Test/src/FilteringAnArray.java
@@ -0,0 +1,31 @@
+/*
+ * Jonathan Graydon
+ * Filtering An Array
+ * This program will take in 10 strings from the user
+ * Then it will print only the strings with an even number of characters
+ */
+
+ import java.util.Scanner;
+
+
+public class FilteringAnArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] array = new String[10];
+
+        for (int i = 0; i<=9; i++){
+            System.out.println("Enter string " + (i+1));
+            array[i] = sc.nextLine();
+        }
+        System.out.println("The even length strings are: ");
+        for (int i = 0; i< array.length; i++){
+            if(array[i].length()%2 == 0){
+                System.out.println(array[i]);
+            }
+            else{
+                continue;
+            }
+        }
+    }
+}
