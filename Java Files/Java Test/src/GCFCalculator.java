7e72391e12119a3b7ef90dc8436f60e120b37559
Romgi Productions <85715209+Romgi@users.noreply.github.com>
2024-11-12 13:32:24 -0500

    started gcf calculator

--- Java Files/Java Test/src/GCFCalculator.java
+++ Java Files/Java Test/src/GCFCalculator.java
@@ -4,23 +4,40 @@
  * This program will take in two integer inputs, and return the GCF
  */
 
- import java.util.Scanner;
-
 public class GCFCalculator {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter the first integer: "); int num1 = sc.nextInt();
-        System.out.println("Enter the second integer: "); int num2 = sc.nextInt();
+        int num1 = 16;
+        int num2 = 32;
+        System.out.println("The gcf is: " + calculateGCf(num1, num2));
 
+        num2 = 162;
+        num1 = 263;
+        System.out.println("The gcf is: " + calculateGCf(num1, num2));
 
     }
 
     public static Integer calculateGCf(int num1, int num2){
+        int gf1 = 0;
+        int gf2 = 0;
         int gcf = 0;
 
-        
-
+        while (true){
+            for (int i = 1; i < num1; i++){
+                if(num1 % i == 0){
+                    if(i > gf1){
+                        gf1 = i;
+                    }
+                }
+            }
+            for (int i = 1; i < num2; i++){
+                if(num2 % i == 0){
+                    if(i > gf2){
+                        gf2 = i;
+                    }
+                }
+            }
+        }
         return gcf;
     }
 }
