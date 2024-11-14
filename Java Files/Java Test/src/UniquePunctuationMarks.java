d08525b8c15361abbe1330400bbb4e5834b51c13
Romgi Productions <85715209+Romgi@users.noreply.github.com>
2024-11-14 13:09:05 -0500

    fixed bugs in unique punctuation

--- Java Files/Java Test/src/UniquePunctuationMarks.java
+++ Java Files/Java Test/src/UniquePunctuationMarks.java
@@ -4,28 +4,32 @@
  * This program will take in a sentence and print the amount of unique punctuation marks
  */
 
- import java.util.Scanner;
- import java.util.Set;
- import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.HashSet;
 
 public class UniquePunctuationMarks {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        //User Input
         System.out.println("Enter the sentence: ");
         String sentence = sc.nextLine();
 
+        //List of values
         String[] listOfPunctuation = {",", "?", "!", ";", ":", "-"};
-
         Set<Character> set_of_unique_punctuation = new HashSet<>();
 
-        for (int i = 0; i < sentence.length(); i++){
-            for (int j = 0; j < listOfPunctuation.length; i++){
-                if (sentence.substring(i, i).equals(listOfPunctuation[j])){
+        //Compare the sentence to the list of punctuation
+        for (int i = 0; i < sentence.length(); i++) {
+            for (int j = 0; j < listOfPunctuation.length; j++) {
+                if (sentence.charAt(i) == listOfPunctuation[j].charAt(0)) {
+                    //If punctuation is detected, add it to the set
                     set_of_unique_punctuation.add(sentence.charAt(i));
                 }
             }
         }
-        System.out.println("There are: " + set_of_unique_punctuation.size() + "different types of punctuation.");
+        //Output results
+        System.out.println("There are: " + set_of_unique_punctuation.size() + " different types of punctuation.");
     }
 }
