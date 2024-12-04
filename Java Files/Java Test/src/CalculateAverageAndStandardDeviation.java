3c3c1a6c19c577d48ed0c8ee3e330a4c864fba06
Romgi Productions <85715209+Romgi@users.noreply.github.com>
2024-12-04 13:05:54 -0500

    added new number example

---
+++ Java Files/Java Test/src/CalculateAverageAndStandardDeviation.java
@@ -0,0 +1,11 @@
+/*
+ * Jonathan Graydon
+ * Average and Standard Deviation
+ * This program will calculate the average and standard deviation from a text file with numbers in it
+ */
+
+public class CalculateAverageAndStandardDeviation {
+    public static void main(String[] args) {
+        
+    }
+}
