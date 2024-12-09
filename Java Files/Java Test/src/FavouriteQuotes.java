a0203aefdeca0a74d1c27fea448b31a6a11b2eb0
Romgi Productions <JonathanGraydon22@gmail.com>
2024-12-08 19:31:39 -0500

    finished quotes program and changed file structure

--- Java Files/Java Test/src/FavouriteQuotes.java
+++ Java Files/Java Test/src/FavouriteQuotes.java
@@ -4,14 +4,30 @@
  * This program will collect quotes from the user and save them in a text file
  */
 
- import java.io.BufferedReader;
- import java.io.FileReader;
- import java.io.IOException;
- import java.util.Scanner;;
+ import java.io.*;
+ import java.util.Scanner;
 
 public class FavouriteQuotes {
     public static void main(String[] args) throws IOException{
         Scanner sc = new Scanner(System.in);
-        
+        int startChoice = 1;
+        while (startChoice == 1) { 
+        System.out.println("Do you want to enter a quote? Yes [1] No[2]: ");
+        startChoice = sc.nextInt();
+
+            switch (startChoice){
+                case 1: System.out.println("Enter the quote to add: ");
+                String quoteToAdd = sc.nextLine();
+                quoteToAdd = sc.nextLine();
+                addQuote(quoteToAdd);
+                break;
+                default: System.out.println("Quitting program...");
+            }
+        }
+    }
+    public static void addQuote(String quote) throws IOException{
+        PrintWriter writer = new PrintWriter("Quotes.txt");
+        writer.println(quote);
+        writer.close();
     }
 }
