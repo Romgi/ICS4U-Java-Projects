4381ea1e8e86457419503c9347b93315934617c3
Romgi Productions <JonathanGraydon22@gmail.com>
2024-12-08 19:36:49 -0500

    initialized contact book using file storage

---
+++ Java Files/Java Test/src/ContactBookFile.java
@@ -0,0 +1,12 @@
+/*
+ * Jonathan Graydon
+ * Contact Book With File Storage
+ * This program will save the name, age, and phone number entered for each contact
+ * The program will then write the data into a .txt file for storage
+ */
+
+public class ContactBookFile {
+    public static void main(String[] args) {
+        
+    }
+}
